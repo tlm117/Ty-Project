a6eb88906bad5432b250905373387940411da49f
tlm117 <59628972+tlm117@users.noreply.github.com>
2020-12-06 23:00:53 -0500

    worked on new signs and added animals etc

---
+++ Assets/SmokeAndAway.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmokeAndAway : MonoBehaviour
+{
+    public GameObject SmokePuff;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        Transform thisSpot = this.GetComponent<Transform>();
+        Debug.Log(thisSpot);
+        Instantiate(SmokePuff, thisSpot);
+        Destroy(this);
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Transform thisSpot = this.GetComponent<Transform>();
+        Debug.Log(thisSpot);
+        Instantiate(SmokePuff, thisSpot);
+        Destroy(this);
+    }
+}
